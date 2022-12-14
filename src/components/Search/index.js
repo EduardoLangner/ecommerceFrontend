ac4531594404ba61e526b1bcad580c25f5d7197b
EduardoLangner <eduardodossantos752@gmail.com>
2022-12-14 16:31:17 -0300

    feat: add clearinput

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import  './index.css'
 
 const Search = () => {
+
+    const [clear, setClear] = useState('inactive')
+
 
     return (
         <div className="search">
             <div className="search-input">
                 <div className="search-field">
-                    <input className="form" type="text" placeholder="Pesquisar..." />
+                    <input className="form" type="text" placeholder="Pesquisar..." onInput={()=>{setClear('active')}} onBlur={()=>{setClear('inactive')}}/>
+                </div>
+                <div className="clear">
+                    <i className={`bi bi-x icon-${clear}`}></i>
                 </div>
                 <div className="icon"> 
                     <i className="bi bi-search icon-search"></i>
