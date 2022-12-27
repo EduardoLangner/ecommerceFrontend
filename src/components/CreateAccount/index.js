4a8eda242b7ad18c72da7a8012521176663bf5a4
EduardoLangner <eduardodossantos752@gmail.com>
2022-12-27 15:54:47 -0300

    feat: add validation that inputs

--- src/components/CreateAccount/index.js
+++ src/components/CreateAccount/index.js
@@ -13,22 +13,72 @@ const CreateAccount = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+
     const [nameError, setNameError] = useState(false)
     const [cpfError, setCPFError] = useState(false)
     const [telephoneError, setTelephoneError] = useState(false)
     const [emailError, setEmailError] = useState(false)
     const [passwordError, setPasswordError] = useState(false)
-    const [confirmPasswordError, setConfirmPasswordError] = useState(false)
 
     const validate = () => {
-        !validateName.test(name) ? setNameError(true) : setNameError(false)
-        !validateCPF.test(cpf) ? setCPFError(true) : setCPFError(false)
-        !validateTelephone.test(telephone) ? setTelephoneError(true) : setTelephoneError(false)
-        !validadeEmail.test(email) ? setEmailError(true) : setEmailError(false)
-        !validatePassword.test(password) ? setPasswordError(true) : setPasswordError(false)
-        !validateConfirmPassword.test(confirmPassword) ? setConfirmPasswordError(true) : setConfirmPasswordError(false)
-    }        
+        
+        if(!validateName.test(name) || name === '') {
+            setNameError(true)
+            document.querySelector('.signup-input-name').style.borderBottom = '1.5px solid red'
+        } else {
+            setNameError(false)
+            document.querySelector('.signup-input-name').style.borderBottom = '1.5px solid #000'
+        }
 
+        if(!validateCPF.test(cpf) || cpf === '') {
+            setCPFError(true)
+            document.querySelector('.signup-input-cpf').style.borderBottom = '1.5px solid red'
+        } else {
+            setCPFError(false)
+            document.querySelector('.signup-input-cpf').style.borderBottom = '1.5px solid #000'
+        }
+
+        if(!validateTelephone.test(telephone) || telephone === '') {
+            setTelephoneError(true)
+            document.querySelector('.signup-input-telephone').style.borderBottom = '1.5px solid red'
+        } else {
+            setTelephoneError(false)
+            document.querySelector('.signup-input-telephone').style.borderBottom = '1.5px solid #000'
+        }
+
+        if(!validadeEmail.test(email) || email === '') {
+            setEmailError(true)
+            document.querySelector('.signup-input-email').style.borderBottom = '1.5px solid red'
+        } else {
+            setEmailError(false)
+            document.querySelector('.signup-input-email').style.borderBottom = '1.5px solid #000'
+        }
+
+        if(!validatePassword.test(password) || password === '') {
+            setPasswordError(true)
+            document.querySelector('.signup-input-password').style.borderBottom = '1.5px solid red'
+        } else {
+            setPasswordError(false)
+            document.querySelector('.signup-input-password').style.borderBottom = '1.5px solid #000'
+        }
+
+        if(!validateConfirmPassword.test(confirmPassword)){
+            setPasswordError(true)
+            document.querySelector('.signup-input-confirm-password').style.borderBottom = '1.5px solid red'
+        } else if (password !== confirmPassword || confirmPassword === '') {
+            setPasswordError(true)
+            document.querySelector('.signup-input-confirm-password').style.borderBottom = '1.5px solid red'
+        } else {
+            setPasswordError(false)
+            document.querySelector('.signup-input-confirm-password').style.borderBottom = '1.5px solid #000'
+        }
+    }
+        // !validateName.test(name) ? setNameError(true) : setNameError(false)
+        // !validateCPF.test(cpf) ? setCPFError(true) : setCPFError(false)
+        // !validateTelephone.test(telephone) ? setTelephoneError(true) : setTelephoneError(false)
+        // !validadeEmail.test(email) ? setEmailError(true) : setEmailError(false)
+        // !validatePassword.test(password) ? setPasswordError(true) : setPasswordError(false)
+           
     return (
         <div className='create-account'>
             <div className='create-account-content'>
@@ -37,36 +87,65 @@ const CreateAccount = () => {
                 </div>
                 <div className='create-account-form'>
                     <form className='inputs-signup'>
+
                         <div className='signup-input-name'>
-                            <i className='bi bi-person icon-name'><input type='text' placeholder='Nome completo*' className='input-name' 
-                                value={name} onChange={(e) => {setName(e.target.value)}}></input></i>
-                        </div>
-                        {nameError && <p className='name-error'>Nome inválido</p>}
+                            <i className='bi bi-person icon-name'>
+                                <input type='text' placeholder='Nome completo*' 
+                                    className='input-name' 
+                                    value={name} 
+                                    onChange={(e) => {setName(e.target.value)}}>
+                                </input>
+                            </i>
+                        </div>{nameError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='name-error'>Nome inválido</p></i></div>}
+
                         <div className='signup-input-cpf'>
-                            <i className='bi bi-person-vcard icon-cpf'><input type='text' placeholder='CPF (000.000.000-00)*' className='input-cpf' 
-                                value={cpf} onChange={(e) => {setCPF(e.target.value)}}></input></i>
-                        </div>
-                        {cpfError && <p className='cpf-error'>CPF inválido</p>}
+                            <i className='bi bi-person-vcard icon-cpf'>
+                                <input type='text' placeholder='CPF (000.000.000-00)*' 
+                                    className='input-cpf' 
+                                    value={cpf} 
+                                    onChange={(e) => {setCPF(e.target.value)}}>
+                                </input>
+                            </i>
+                        </div>{cpfError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='cpf-error'>CPF inválido</p></i></div>}
+
                         <div className='signup-input-telephone'>
-                            <i className='i class="bi bi-telephone icon-telephone'><input type='tel' placeholder='Telefone celular*' className='input-telephone' 
-                                value={telephone} onChange={(e) => {setTelephone(e.target.value)}}></input></i>
-                        </div>
-                        {telephoneError && <p className='telephone-error'>Telefone inválido</p>}
+                            <i className='i className="bi bi-telephone icon-telephone'>
+                                <input type='tel' placeholder='Telefone celular*' className='input-telephone' 
+                                    value={telephone}  
+                                    onChange={(e) => {setTelephone(e.target.value)}}>
+                                </input>
+                            </i>
+                        </div>{telephoneError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='telephone-error'>Telefone inválido</p></i></div>}
+                       
                         <div className='signup-input-email'>
-                            <i className='bi bi-envelope icon-email'><input type='email' placeholder='E-mail*' className='input-email' 
-                                value={email} onChange={(e) => {setEmail(e.target.value)}}></input></i>
-                        </div>
-                        {emailError && <p className='email-error'>E-mail inválido</p>}
+                            <i className='bi bi-envelope icon-email'>
+                                <input type='email' placeholder='E-mail*' 
+                                    className='input-email' 
+                                    value={email} 
+                                    onChange={(e) => {setEmail(e.target.value)}}>
+                                    </input>
+                                </i>
+                        </div>{emailError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='email-error'>E-mail inválido</p></i></div>}
+                        
                         <div className='signup-input-password'>
-                            <i className='bi bi-lock icon-password'><input type='password' placeholder='Crie sua senha*' className='input-password' 
-                                value={password} onChange={(e) => {setPassword(e.target.value)}}></input></i>
+                            <i className='bi bi-lock icon-password'>
+                                <input type='password' placeholder='Crie sua senha*' 
+                                    className='input-password' 
+                                    value={password} 
+                                    onChange={(e) => {setPassword(e.target.value)}}>
+                                </input>
+                            </i>
+                        </div>{passwordError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='password-error'>Senha inválida</p></i></div>}
+                        
+                        <div className='signup-input-confirm-password'>
+                            <i className='bi bi-lock icon-password'>
+                                <input type='password' placeholder='Confirme sua senha*' 
+                                    className='input-password' 
+                                    value={confirmPassword} 
+                                    onChange={(e) => {setConfirmPassword(e.target.value)}}>
+                                </input>
+                            </i>
                         </div>
-                        {passwordError && <p className='password-error'>Senha inválida</p>}
-                        <div className='signup-input-confirmPassword'>
-                            <i className='bi bi-lock icon-password'><input type='password' placeholder='Confirme sua senha*' className='input-password' 
-                                value={confirmPassword} onChange={(e) => {setConfirmPassword(e.target.value)}}></input></i>
-                        </div>
-                        {confirmPasswordError && <p className='confirm-password-error'>Senha inválida</p>}
                     </form>
                     <button className='create-account-button' type='submit' onClick={validate}>Criar conta</button>
                 </div>
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,4 +1,4 @@
-export const validateName = new RegExp(/\b([A-ZÀ-ÿa-z][-,a-z. ']+[ ]*)+/)
+export const validateName = new RegExp(/\b[A-Za-zÀ-ú][A-Za-zÀ-ú]+,?\s[A-Za-zÀ-ú][A-Za-zÀ-ú]{2,19}\b/);
 export const validateCPF = new RegExp(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/)
 export const validateTelephone = new RegExp(/^[1-9]{2}(?:[2-8]|9[1-9])[0-9]{3}[0-9]{4}$/)
 export const validadeEmail = new RegExp(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]{3}$/i)
