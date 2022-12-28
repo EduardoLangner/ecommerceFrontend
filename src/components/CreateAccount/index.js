597b85d0c20c39ce1a04382a6e4a8ca1d6884767
EduardoLangner <eduardodossantos752@gmail.com>
2022-12-28 16:51:17 -0300

    feat: add validation colors for the inputs

--- src/components/CreateAccount/index.js
+++ src/components/CreateAccount/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
-import { validateName, validateCPF, validateTelephone, validadeEmail, validatePassword, validateConfirmPassword } from '../../utils/regex.js'
+import { validateName, validateCPF, validateTelephone, validadeEmail } from '../../utils/regex.js'
+import validator from 'validator'
 import './index.css'
 
 const CreateAccount = () => {
@@ -13,12 +14,11 @@ const CreateAccount = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
-
     const [nameError, setNameError] = useState(false)
     const [cpfError, setCPFError] = useState(false)
     const [telephoneError, setTelephoneError] = useState(false)
+    const [confirmPasswordError, setConfirmPasswordError] = useState(false)
     const [emailError, setEmailError] = useState(false)
-    const [passwordError, setPasswordError] = useState(false)
     
     const validate = () => {
         
@@ -54,23 +54,45 @@ const CreateAccount = () => {
             document.querySelector('.signup-input-email').style.borderBottom = '1.5px solid green'
         }
 
-        if(!validatePassword.test(password) || password === '') {
-            setPasswordError(true)
-            document.querySelector('.signup-input-password').style.borderBottom = '1.5px solid red'
-        } else {
-            setPasswordError(false)
-            document.querySelector('.signup-input-password').style.borderBottom = '1.5px solid green'
-        }
-
-        if(!validateConfirmPassword.test(confirmPassword)){
-            setPasswordError(true)
-            document.querySelector('.signup-input-confirm-password').style.borderBottom = '1.5px solid red'
-        } else if (password !== confirmPassword || confirmPassword === '') {
-            setPasswordError(true)
+        if(confirmPassword !== password || confirmPassword === '') {
+            setConfirmPasswordError(true)
             document.querySelector('.signup-input-confirm-password').style.borderBottom = '1.5px solid red'
         } else {
-            setPasswordError(false)
+            setConfirmPasswordError(false)
             document.querySelector('.signup-input-confirm-password').style.borderBottom = '1.5px solid green'
+        }
+    }
+
+    const validatePassword = (value) => {
+        
+        if(validator.isStrongPassword(value, {minLength: 8})) {
+            document.querySelector('.characters').style.color = 'green'
+        } else {
+            document.querySelector('.characters').style.color = 'red'
+        }        
+        
+        if(validator.isStrongPassword(value, {minLowercase: 1})) {
+            document.querySelector('.lowercase').style.color = 'green'
+        } else {
+            document.querySelector('.lowercase').style.color = 'red'
+        }
+        
+        if(validator.isStrongPassword(value, {minUppercase: 1})) {
+            document.querySelector('.uppercase').style.color = 'green'
+        } else {
+            document.querySelector('.uppercase').style.color = 'red'
+        }
+        
+        if(validator.isStrongPassword(value, {minNumbers: 1})) {
+            document.querySelector('.numbers').style.color = 'green'
+        } else {
+            document.querySelector('.numbers').style.color = 'red'
+        }
+        
+        if(validator.isStrongPassword(value, {minSymbols: 1})) {
+            document.querySelector('.symbols').style.color = 'green'
+        } else {
+            document.querySelector('.symbols').style.color = 'red'
         }
     }
            
@@ -92,7 +114,7 @@ const CreateAccount = () => {
                                     onChange={(e) => {setName(e.target.value)}}>
                                 </input>
                             </i>
-                        </div>{nameError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='name-error'>Nome inválido</p></i></div>}
+                        </div>{nameError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='name-error'>Por favor, insira seu nome completo.</p></i></div>}
                         <div  className='signup-input-cpf'>
                             <i className='bi bi-person-vcard icon-cpf'>
                                 <input type='text' placeholder='CPF*'
@@ -101,18 +123,18 @@ const CreateAccount = () => {
                                     onChange={(e) => {setCPF(e.target.value)}}>
                                 </input>
                             </i>
-                        </div>{cpfError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='cpf-error'>CPF inválido</p></i></div>}
+                        </div>{cpfError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='cpf-error'>O CPF inserido não é inválido</p></i></div>}
 
 
                         <div className='signup-input-telephone'>
                             <i className='i className="bi bi-telephone icon-telephone'>
-                                <input type='tel' placeholder='Telefone celular*' className='input-telephone' 
+                                <input type='tel' placeholder='Telefone*' className='input-telephone' 
                                     value={telephone}  
                                     maxLength='15'
                                     onChange={(e) => {setTelephone(e.target.value)}}>
                                 </input>
                             </i>
-                        </div>{telephoneError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='telephone-error'>Telefone inválido</p></i></div>}
+                        </div>{telephoneError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='telephone-error'>O telefone inserido não é inválido</p></i></div>}
                        
                         <div className='signup-input-email'>
                             <i className='bi bi-envelope icon-email'>
@@ -122,18 +144,36 @@ const CreateAccount = () => {
                                     onChange={(e) => {setEmail(e.target.value)}}>
                                     </input>
                                 </i>
-                        </div>{emailError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='email-error'>E-mail inválido</p></i></div>}
+                        </div>{emailError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='email-error'>O E-mail inserido não é inválido</p></i></div>}
                         
                         <div className='signup-input-password'>
                             <i className='bi bi-lock icon-password'>
                                 <input type='password' placeholder='Crie sua senha*' 
                                     className='input-password' 
-                                    value={password} 
+                                    value={password}
                                     onChange={(e) => {setPassword(e.target.value)}}>
                                 </input>
                             </i>
-                        </div>{passwordError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='password-error'>Senha inválida</p></i></div>}
-                        
+                        </div>
+                        <div className='requisites-password'>
+                            <p className='title'>A senha deve atender os seguintes requisitos: </p><br />
+                            <div className='requisites-characters'>
+                                <p className='characters'>● No mínimo 8 caracteres</p>
+                            </div>
+                            <div className='requisites-uppercase'>
+                                <p className='uppercase'>● pelo menos 1 letra maiúscula (A...Z)</p>
+                            </div>
+                            <div className='requisites-lowercase'>
+                                <p className='lowercase'>● pelo menos 1 letra minúscula (a...z)</p>
+                            </div>
+                            <div className='requisites-number'>
+                                <p className='numbers'>● pelo menos 1 número (0...9)</p>
+                            </div>
+                            <div className='requisites-symbol'>
+                                <p className='symbols'>● pelo menos 1 caractere especial</p>
+                            </div>
+                        </div>
+
                         <div className='signup-input-confirm-password'>
                             <i className='bi bi-lock icon-password'>
                                 <input type='password' placeholder='Confirme sua senha*' 
@@ -142,7 +182,7 @@ const CreateAccount = () => {
                                     onChange={(e) => {setConfirmPassword(e.target.value)}}>
                                 </input>
                             </i>
-                        </div>
+                        </div>{confirmPasswordError && <div className='error'><i className="bi bi-exclamation-triangle triangle"><p className='email-error'>As senhas não conferem</p></i></div>}
                     </form>
                     <button className='create-account-button' type='submit' onClick={validate}>Criar conta</button>
                 </div>
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -2,10 +2,3 @@ export const validateName = new RegExp(/\b[A-Za-zÀ-ú][A-Za-zÀ-ú]+,?\s[A-Za-z
 export const validateCPF = new RegExp(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/)
 export const validateTelephone = new RegExp(/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/)
 export const validadeEmail = new RegExp(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]{3}$/i)
-export const validatePassword = new RegExp(/(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
-export const validateConfirmPassword = new RegExp(/(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
-// O tamanho da senha deve ser maior ou igual a 8
-// A senha deve conter um ou mais caracteres maiúsculos
-// A senha deve conter um ou mais caracteres minúsculos
-// A senha deve conter um ou mais valores numéricos
-// A senha deve conter um ou mais caracteres especiais
